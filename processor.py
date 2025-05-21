673b5a7b0c6f505a924a9839151ada39df35bfcc
Mateus Santana Joaquim <44446910+mateusbrazza@users.noreply.github.com>
2025-05-21 08:55:39 -0300

    Add files via upload

---
+++ processor.py
@@ -0,0 +1,45 @@
+def consolidate_payloads(payloads):
+    total_pf = 0
+    total_pj = 0
+    organization_map = {}
+
+    for payload in payloads:
+        total_pf += payload.totalPF
+        total_pj += payload.totalPJ
+
+        for org in payload.organizations:
+            if org.id not in organization_map:
+                organization_map[org.id] = {
+                    "id": org.id,
+                    "name": org.name,
+                    "initiators": {}
+                }
+
+            for init in org.initiators:
+                if init.id not in organization_map[org.id]["initiators"]:
+                    organization_map[org.id]["initiators"][init.id] = {
+                        "id": init.id,
+                        "name": init.name,
+                        "authorized": 0,
+                        "revoked": 0
+                    }
+
+                if init.status == "AUTHORIZED":
+                    organization_map[org.id]["initiators"][init.id]["authorized"] += init.total
+                elif init.status == "REVOKED":
+                    organization_map[org.id]["initiators"][init.id]["revoked"] += init.total
+
+    summarized_orgs = []
+    for org in organization_map.values():
+        summarized_orgs.append({
+            "id": org["id"],
+            "name": org["name"],
+            "initiators": list(org["initiators"].values())
+        })
+
+    return {
+        "totalPF": total_pf,
+        "totalPJ": total_pj,
+        "organizationCount": len(summarized_orgs),
+        "organizations": summarized_orgs
+    }
